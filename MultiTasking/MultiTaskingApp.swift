682c099fe7104ba2c5d21d7ce6dba7f3a6b8325f
0x67 <evilisnj@gmail.com>
2025-08-30 18:15:49 -0700

    Initial Commit

---
+++ MultiTasking/MultiTaskingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTaskingApp.swift
+//  MultiTasking
+//
+//  Created by 0x67 on 2025-08-30.
+//
+
+import SwiftUI
+
+@main
+struct MultiTaskingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
